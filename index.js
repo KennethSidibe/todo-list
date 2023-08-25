986175cb95eb1189149bd04286a5123789297eeb
Sidibe <Elstevehcks@gmail.com>
2023-08-24 21:38:30 -0400

    Integrate task add functionnality

--- index.js
+++ index.js
@@ -3,17 +3,37 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import * as Quote from "inspirational-quotes";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+var tasks = ["Wash the dishes", "Send letter"];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}.`);
+  console.log(`Listening on port ${port}.`);
 });
 
-app.get('/', (req, res) => {
-    res.render(__dirname + '/views/index.ejs');
+app.get("/", (req, res) => {
+    var quoteObj = Quote.getQuote();
+    var quote = quoteObj['text'];
+    var author = quoteObj['author'];
+
+    console.log("quote:",quote);
+    console.log("author:", author);
+    res.render(__dirname + "/views/index.ejs", {
+        tasks: tasks,
+        quote:quote,
+        author:author  
+        });
+});
+
+app.post("/add-task", (req, res) => {
+    tasks.push(req.body.newTask);
+    res.render(__dirname + "/views/index.ejs", {
+        tasks:tasks
+    });
 });
---
+++ public/scripts/script.js
@@ -0,0 +1,26 @@
+$('.task-checkbox').change(function() {
+    if ($(this).prop('checked')) {
+        $(this).parent().last().css("text-decoration", "line-through");
+    } else {
+        $(this).parent().last().css("text-decoration", "none");
+    }
+});
+
+function getCurrentDayFormatted() {
+    const date = new Date();
+    
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  
+    const dayName = daysOfWeek[date.getDay()];
+    const dayOfMonth = date.getDate();
+    const monthName = months[date.getMonth()];
+  
+    var dateText = `${dayName}, ${dayOfMonth} ${monthName}`;
+    $('#currentDate').text(dateText);
+}
+
+var date = getCurrentDayFormatted();
+  
+
+$('.task-container').last().parent().addClass("pb-3");
